bbc6d04b7e4cf0ea385a6ef4688033a0f4c2176a
Joshua K Bader <jaybshinsen@hotmail.com>
2020-04-06 05:07:36 +0800

    website is done

---
+++ script.js
@@ -0,0 +1,32 @@
+$(document).ready(function(){
+
+var slideIndex = 0;
+var abort = false;
+carousel();
+
+// var slideIndex = 1;
+// showDivs(slideIndex);
+
+// function plusSlides(n) {
+//   showDivs(slideIndex += n);
+// }
+
+function carousel() {
+  if (abort) {
+    return;
+  } else {
+  var i;
+  var x = $(".mySlides");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > x.length) {slideIndex = 1}
+  x[slideIndex-1].style.display = "block";
+  setTimeout(carousel, 5000); // Change image every 5 seconds
+
+}
+}
+
+
+});
