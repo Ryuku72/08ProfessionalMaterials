7817e3b532297422706d84c7c73393d7453f3059
Joshua K Bader <jaybshinsen@hotmail.com>
2020-04-10 03:16:44 +0800

    changed top screen and removed scroll bar

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 $(document).ready(function(){
 
-var slideIndex = 0;
-var abort = false;
-carousel();
+// var slideIndex = 0;
+// var abort = false;
+// carousel();
 
 // var slideIndex = 1;
 // showDivs(slideIndex);
@@ -11,22 +11,32 @@ carousel();
 //   showDivs(slideIndex += n);
 // }
 
-function carousel() {
-  if (abort) {
-    return;
-  } else {
-  var i;
-  var x = $(".mySlides");
-  for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";
-  }
-  slideIndex++;
-  if (slideIndex > x.length) {slideIndex = 1}
-  x[slideIndex-1].style.display = "block";
-  setTimeout(carousel, 5000); // Change image every 5 seconds
+// function carousel() {
+//   if (abort) {
+//     return;
+//   } else {
+//   var i;
+//   var x = $(".mySlides");
+//   for (i = 0; i < x.length; i++) {
+//     x[i].style.display = "none";
+//   }
+//   slideIndex++;
+//   if (slideIndex > x.length) {slideIndex = 1}
+//   x[slideIndex-1].style.display = "block";
+//   setTimeout(carousel, 5000); // Change image every 5 seconds
 
-}
-}
+// }
+// }
 
+$("#project").click(function(){
+  $("#section").removeClass("hide");
+  $('html,body').animate({
+    scrollTop: $(".week1").offset().top},
+    'slow');
+})
+
+$("#media").click(function(){
+  $("#section").addClass("hide");
+})
 
 });
