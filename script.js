b46c01e43781a982a0ba60eaf062bb7203c8d8c8
Joshua K Bader <jaybshinsen@hotmail.com>
2020-04-12 22:10:52 +0800

    removed js script for carousel

--- script.js
+++ script.js
@@ -1,33 +1,5 @@
 $(document).ready(function(){
 
-// var slideIndex = 0;
-// var abort = false;
-// carousel();
-
-// var slideIndex = 1;
-// showDivs(slideIndex);
-
-// function plusSlides(n) {
-//   showDivs(slideIndex += n);
-// }
-
-// function carousel() {
-//   if (abort) {
-//     return;
-//   } else {
-//   var i;
-//   var x = $(".mySlides");
-//   for (i = 0; i < x.length; i++) {
-//     x[i].style.display = "none";
-//   }
-//   slideIndex++;
-//   if (slideIndex > x.length) {slideIndex = 1}
-//   x[slideIndex-1].style.display = "block";
-//   setTimeout(carousel, 5000); // Change image every 5 seconds
-
-// }
-// }
-
 $("#project").click(function(){
   $("#section").removeClass("hide");
   $('html,body').animate({
